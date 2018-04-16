5ef3a658d31e2f043e85def8d6e3847f436770cb
suchov <artem.suchov@gmail.com>
2018-04-16 20:14:19 +0300

    finish this shorty, it's rather simple with the webpack

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # README
 
 [engineyard.com rails and vue js](https://www.engineyard.com/blog/rails-and-vue-js-part-1)
+
+Running bin/webpack-dev-server
+can speed up a bit separate pages, not running rails, just webpack
+
+more info about vue in the official guide [vuejs.org](https://vuejs.org/v2/guide/index.html)
--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -8,24 +8,60 @@
 import Vue from 'vue/dist/vue.esm'
 
 document.addEventListener('DOMContentLoaded', () => {
+  // simple data
   var app = new Vue({
     el: '#app',
     data: {
       message: 'Hello Vue!'
     }
   });
+  // title that showing on hover
   var app2 = new Vue({
     el: '#app-2',
     data: {
       message: 'You loaded this page on ' + new Date().toLocaleString()
     }
   });
+  // if statement for showing data
   var app3 = new Vue({
     el: '#app-3',
     data: {
       seen: true
     }
   });
+  // loop with for through the array
+  var app4 = new Vue({
+    el: '#app-4',
+    data: {
+      todos: [
+        { text: 'Learn JavaScript' },
+        { text: 'Learn Vue' },
+        { text: 'Build something awesome' }
+      ]
+    }
+  });
+  // methods to event listeners
+  // The reverseMessage function goes inside methods.
+  var app5 = new Vue({
+    el: '#app-5',
+    data: {
+      message: 'Hello Vue.js!'
+    },
+    methods: {
+      reverseMessage: function () {
+        this.message = this.message.split('').reverse().join('');
+      }
+    }
+  });
+  //
+  var app6 = new Vue({
+    el: '#app-6',
+    data: {
+      message: 'Hello Vue!'
+    }
+  });
+  //
+  
 });
 
 
